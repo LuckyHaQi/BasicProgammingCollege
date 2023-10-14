9cc74840c593de5a07e24ec887acdeaa87710ca6
HaQi <143992115+LuckyHaQi@users.noreply.github.com>
2023-10-15 01:42:09 +0700

    1

--- PemilihanPercobaan216.java
+++ PemilihanPercobaan216.java
@@ -14,7 +14,24 @@ public class PemilihanPercobaan216{
 
         float total = (uas * 0.4F ) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
         String message = total < 65 ? "Anda dinyatakan remidi" : " Anda tidak dinyatakan remidi";
-        System.out.println("Nilai akhir " + total + " sehingga " + message);
+        //System.out.println("Nilai akhir " + total + " sehingga " + message);
 
+        String predikat;
+        if (100 >= total && total > 80){
+                predikat = " A " ;
+        }   else if (80 >= total && total > 73) {
+                predikat = " B+ ";
+        }   else if (73 >= total && total > 65) {
+                predikat = " B " ;
+        }   else if (65 >= total && total > 60) {
+                predikat = " C+ ";
+        }   else if (60 >= total && total > 50) {
+                predikat = " C " ;
+        }   else if (50 >= total && total > 39) {
+                predikat = " D " ;
+        }   else {
+                predikat = " E " ;
+        }
+        System.out.println("Nilai total yang didapat " + total + " sehingga mendapat predikat" + predikat);
     }
 }
