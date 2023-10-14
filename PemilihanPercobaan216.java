85a4a2e1fde0c6dd1f79e954107a635ebe5ec4b1
HaQi <143992115+LuckyHaQi@users.noreply.github.com>
2023-10-15 01:38:03 +0700

    Add files via upload

---
+++ PemilihanPercobaan216.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PemilihanPercobaan216{
+    public static void main (String[] args){
+        Scanner input16 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai UAS   : ");
+        float uas = input16.nextFloat();
+        System.out.print("Masukkan nilai UTS   : ");
+        float uts = input16.nextFloat();
+        System.out.print("Masukkan Nilai Kuis  : ");
+        float kuis = input16.nextFloat();
+        System.out.print("Masukkan Nilai Tugas : ");
+        float tugas = input16.nextFloat();
+
+        float total = (uas * 0.4F ) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Anda dinyatakan remidi" : " Anda tidak dinyatakan remidi";
+        System.out.println("Nilai akhir " + total + " sehingga " + message);
+
+    }
+}
