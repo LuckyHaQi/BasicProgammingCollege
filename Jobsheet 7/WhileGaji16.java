5f05cba1e5df4586023e655f65f08c1fbb43004f
HaQi <143992115+LuckyHaQi@users.noreply.github.com>
2023-12-04 15:12:53 +0700

    blank

--- Jobsheet 7/WhileGaji16.java
+++ Jobsheet 7/WhileGaji16.java
@@ -10,7 +10,6 @@ public class WhileGaji16{
         jumlahKaryawan = scan.nextInt();
 
         int i = 0;
-
         while (i < jumlahKaryawan){
             System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
             System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
@@ -21,9 +20,9 @@ public class WhileGaji16{
 
             if (jabatan.equalsIgnoreCase("direktur")){
                 continue;
-            } else if (jabatan.equalsIgnoreCase("manajer")){
+            }else if(jabatan.equalsIgnoreCase("manajer")){
                 gajiLembur = jumlahJamLembur * 100000;
-            }else if (jabatan.equalsIgnoreCase("karyawan")){
+            }else if(jabatan.equalsIgnoreCase("karyawan")){
                 gajiLembur = jumlahJamLembur * 75000;
             totalGajiLembur += gajiLembur;
             System.out.println("Total gaji lembur: " + totalGajiLembur);
