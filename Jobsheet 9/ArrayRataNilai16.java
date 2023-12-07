145f54d9ce3518b8620e019bc04b0d6974d75e2f
HaQi <luqmanananta745@gmail.com>
2023-12-07 20:41:47 +0700

    2

--- Jobsheet 9/ArrayRataNilai16.java
+++ Jobsheet 9/ArrayRataNilai16.java
@@ -3,16 +3,21 @@ public class ArrayRataNilai16{
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int [10];
+        int mhsLulus = 0;
         double total = 0;
         double rata2;
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70){
+                mhsLulus += 1;
+            }
         }
         for (int i = 0; i < nilaiMhs.length; i++){
             total += nilaiMhs[i];
         }
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata-rata nilai: " + rata2);
+        System.out.println("Mahasiswa yang lulus: " + mhsLulus + " orang");
     }
 }
