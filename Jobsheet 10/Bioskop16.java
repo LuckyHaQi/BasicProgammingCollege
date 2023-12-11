08410c7ca8355f1791952470f6b0c74b4f277f8b
HaQi <luqmanananta745@gmail.com>
2023-12-11 15:13:43 +0700

    3

--- Jobsheet 10/Bioskop16.java
+++ Jobsheet 10/Bioskop16.java
@@ -1,14 +1,12 @@
 public class Bioskop16{
     public static void main (String[] args){
         String[][] penonton = new String[4][2];
+        
         System.out.println(penonton.length);
-
-        System.out.println();
-        for(int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-"+ (i+1) + ": " + penonton[i].length);
+        for(String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
-        System.out.println();
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
         penonton[1][0] = "Candra";
@@ -23,7 +21,6 @@ public class Bioskop16{
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
         
-
         System.out.println(penonton.length);
         System.out.println(penonton[0].length);
         System.out.println(penonton[1].length);
