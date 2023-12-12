3e70a44bfd460976b6a0ae69fb1a02904fbb8f99
HaQi <luqmanananta745@gmail.com>
2023-12-12 15:48:12 +0700

    3

--- Jobsheet 11/Quiz.java
+++ Jobsheet 11/Quiz.java
@@ -8,7 +8,7 @@ public class Quiz {
         char menu = 'y';
         do {
             int number = random.nextInt(10) + 1;
-            boolean success = false;
+            boolean success = false;     
             do {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = input.nextInt();
@@ -22,8 +22,12 @@ public class Quiz {
                 }
             } while (!success);
             System.out.println("Selamat! Anda benar.");
-            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)? ");
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/y) atau keluar (X/x)? ");
             menu = input.nextLine().charAt(0);
+            if (menu == 'x' || menu == 'X') {
+                System.out.println("Terima kasih sudah bermain. Sampai jumpa!");
+                break; // Keluar dari loop do-while
+            }
         } while (menu == 'y' || menu == 'Y');
     }
 }
