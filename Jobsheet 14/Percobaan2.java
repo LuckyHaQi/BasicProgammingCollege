dd760be26c9ead75bc344c4f6891877436e4d512
HaQi <luqmanananta745@gmail.com>
2024-01-08 14:21:08 +0700

    2

--- Jobsheet 14/Percobaan2.java
+++ Jobsheet 14/Percobaan2.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
+
 public class Percobaan2{
+
     static int hitungPangkat(int x, int y){
         if (y == 0){
-            return(1); 
+            return(1);
         }else{
             return (x * hitungPangkat(x, y-1));
         }
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Bilangan yang dihitung: ");
         int bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         int pangkat = sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+
+        int hasil = 1;
+        for (int i = pangkat; i >= 1; i--){
+            hasil *= bilangan;
+            System.out.print(bilangan + "x");
+        }
+        System.out.print(bilangan + "x1 = " + hasil);
     }
 }
